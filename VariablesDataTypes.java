9917d01a6918782604873b7b2d8af4ca08bcd64f
Devanginisingh2002 <devangini123raghuvanshi@gmail.com>
2024-12-05 17:52:20 +0530

    file

---
+++ VariablesDataTypes.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class VariablesDataTypes{
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+        // Variables: Sum of a + b
+        // int a = 10;
+        // int b = 5;
+        // int c = a + b;
+        // System.out.println(c);
+
+        // sum of two number using input
+        // System.out.print("Input a: ");
+        // int a = sc.nextInt();
+        // System.out.print("Input b: ");
+        // int b = sc.nextInt();
+        // int sum = a + b;
+        // System.out.println("Final output " + sum);
+
+        // Area of Circle: pieR^2
+        // System.out.print("Input Radius: ");
+        // float Radius = sc.nextFloat();
+        // float Area = (3.14f * Radius * Radius);
+        // System.out.println(Area);
+
+
+        //Data Types: Primitive and Non-Primitive
+        /*
+            Primitive:
+            1. byte
+            2. short
+            3. char
+            4. boolean
+            5. int
+            6. long
+            7. float 
+            8. double 
+
+            Non - Primitive:
+            1. String 
+            2. Array
+            3. Class
+            4. Object
+            5. Interface
+        */
+    }
+}
