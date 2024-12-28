7c781cfd95736c4cbeddbd0e24818482960f2106
Devangini Singh <84927553+Devanginisingh2002@users.noreply.github.com>
2024-12-28 15:00:05 +0000

    GetterSetter

---
+++ ObjectOrientedProgramming/GetterSetter3.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class GetterSetter3 {
+  public static void main(String args[]) {
+    Pen p1 = new Pen();
+    p1.setColor("Blue");
+    System.out.println(p1.getColor());
+    p1.setTip(5);
+    System.out.println(p1.getTip());
+
+    // we can change the color
+     p1.setColor("Yellow");
+    System.out.println(p1.getColor()); 
+  }
+}
+
+class Pen {
+  private String color;
+  private int tip;
+
+    //getter 
+    String getColor() {
+        return this.color;
+    }
+    int getTip(){
+        return this.tip;
+    }
+    //setter
+    void setColor(String newColor) { 
+        this.color = newColor; 
+    }
+    void setTip(int tip) {
+        this.tip = tip; 
+    }
+}
