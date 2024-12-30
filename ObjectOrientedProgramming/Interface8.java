8caa02183a10704d40e1290c9e229507ddf6e5b3
Devangini Singh <84927553+Devanginisingh2002@users.noreply.github.com>
2024-12-30 17:20:31 +0000

    Interface

---
+++ ObjectOrientedProgramming/Interface8.java
@@ -0,0 +1,46 @@
+import java.util.*;
+public class Interface8 {
+
+  /********* TOTAL ABSTRACTION *********/
+
+  // public static void main(String args[]){
+  //     Queen Q = new Queen();
+  //     Q.moves();
+  // }
+
+  // interface ChessPlayer {
+  //     void moves();
+  // }
+
+  // class Queen implements ChessPlayer {
+  //     public void moves() {
+  //         System.out.println("up, down, left, right, diagonal in all four
+  //         directions");
+  //     }
+  // }
+  // class Rook implements ChessPlayer {
+  //     public void moves() {
+  //         System.out.println("up, down, left, right");
+  //     }
+  // }
+  // class King implements ChessPlayer {
+  //     public void moves() {
+  //         System.out.println("up, down, left, right, diagonal - step by 1
+  //         direction");
+  //     }
+  // }
+
+  /********* MULTIPLE INHERITANCE *********/
+
+  // interface Herbivore {
+
+  // }
+
+  // interface Carnivore{
+
+  // }
+
+  // class Bear implements Herbivore, Carnivore {
+
+  // }
+}
