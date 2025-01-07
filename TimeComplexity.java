74d36baf3b5ce3d6d18f5fd1cc28833625801bd1
Devangini Singh <84927553+Devanginisingh2002@users.noreply.github.com>
2025-01-07 16:26:49 +0000

    Time Complexity

---
+++ TimeComplexity.java
@@ -0,0 +1,41 @@
+// Time and Space Complexity
+
+/************ TIME COMPLEXITY ***********/
+/* 
+Order Complexity Analysis:
+* Amount of Space or time taken up by an algorithm/Code as function of input
+size.
+* Not the acutal time taken.
+
+* SORTED ARRAY: Perform Constant Operation.
+Case 1 -> Array Size (Increase), Time (Increase)
+Case 2 -> Array Size (Increase), Time is Constant:  MORE EFFICIENT
+
+* For Time Complexity: If we want to check on any program -> We need to see the
+WORST CASE.
+* Linear Search = O(n).  Graph: y = ax + b.
+
+* CONSTANT TIME COMPLEXITY: Find largest and smallest array: TC ->>> O(1).
+
+* 1. BIG O Notation: Upper Bound -> Worst Case T.C
+ (BIG O: If someone ask what's the t.c of the program, Always we need to tell the Big O Notation TC (Worst Case).
+ Graph: an^2 + bn + c.
+
+
+ * 2. BIG OMEGA: Lower Bound T.C
+ (BIG Omega: omega(n^2) is Best Case for this).
+ (Omeag(n) and Omega(1) is not possible).
+
+ * 3. BIG THETA: Average Bound T.C 
+ (Like, Upper is Big 0 and lower is Omega: In between: Average: BIG Theta).
+ (Theta(n^2): Average T.C).
+
+ NOTE: 1. Small O, Small Omega and Small Theta is not important Time Complexity for the Placement purpose.
+       2. Tightly Time complexity: BIG O. and Loosly Time Complexity: Small 0.
+       Example of point 2: Height of the person (If we tell between 6ft to 5ft: TIGHTLY TIME COMPLEXITY),
+                           Height of the person (If we tell between 1inch to 10ft: LOOSLY TIME COMPLEXITY).
+
+*/
+
+/************ SPACE COMPLEXITY **************/
+
