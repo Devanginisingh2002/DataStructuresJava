54682f922b2dc09619d580c6899a79a35df4be3f
Devangini Singh <84927553+Devanginisingh2002@users.noreply.github.com>
2025-01-13 13:12:55 +0000

    Backtracking

--- Backtracking.java
+++ Backtracking.java
@@ -1,68 +1,75 @@
 import java.util.*;
-public class Backtracking {
-    /************* Backtracking on Arrays ***************/
-    // public static void changeArr(int arr[], int i, int val){
-    //     //base case
-    //     if(i == arr.length){
-    //         printArr(arr);
-    //         return;
-    //     }
-    //     //recursion
-    //     arr[i] = val;    // assign value
-    //     changeArr(arr, i+1, val+1);  // function call
-    //     arr[i] = arr[i] - 2;     // backtracking step
-    // }
-
-    // public static void printArr(int arr[]){
-    //     for(int i=0; i<arr.length; i++){
-    //         System.out.print(arr[i] + " ");
-    //     }
-    //     System.out.println();
-    // }
-
-    // public static void main(String args[]){
-    //     int arr[] = new int[5];
-    //     changeArr(arr, 0, 1);
-    //     printArr(arr);
-    // }
-
-    /************ Find Subsets ***********/
-    // public static void findSubsets(String str, String ans, int i){
-    // str: existing string, ans: taking new string for subsets store, and i: index of the string
-    //     // base case
-    //     if(i == str.length()) {
-    //         System.out.println(ans);
-    //         return;
-    //     }
-    //Two choices: Yes and No
-    //     // Yes Choice
-    //     findSubsets(str, ans+str.charAt(i), i+1);    
-    //     // No Choice
-    //     findSubsets(str, ans, i+1);
-    // }
-
-    // public static void main(String args[]){
-    //     String str = "abc";
-    //     findSubsets(str, "", 0);
-    // }
-
-    public static void findPermutation(String str, String ans){
-        // base case
-        if(str.length() == 0){
-            System.out.println(ans);
-            return;
-        }
-        //recursion
-        for(int i=0; i<str.length(); i++){
-            char curr = str.charAt(i);
-            // "abcde" => "ab" + "de" = "abde"
-            str = str.substring(0, i) + str.substring(i+1);
-            findPermutation(str, ans+curr);
-        }
+public class file1 {
+  /*
+  //  Backtracking on Arrays
+  public static void changeArr(int arr[], int i, int val) {
+    // base case
+    if (i == arr.length) {
+      printArr(arr);
+      return;
     }
 
-    public static void main(String args[]){
-        String str = "abc";
-        findPermutation(str, "");
+    // work: recursion
+    arr[i] = val;
+    changeArr(arr, i + 1, val + 1);
+    arr[i] = arr[i] - 2;
+  }
+
+  public static void printArr(int arr[]) {
+    for (int i = 0; i < arr.length; i++) {
+      System.out.print(arr[i] + " ");
     }
+    System.out.println();
+  }
+
+  public static void main(String args[]) {
+    int arr[] = new int[5];
+    changeArr(arr, 0, 1);
+    printArr(arr);
+  } */
+
+  // Subset's String
+  // public static void findSubset(String str, String ans, int i) {
+  //   // base case
+  //   if(i == str.length()){
+  //     if(ans.length() == 0){
+  //       System.out.println("null");
+  //     }else {
+  //       System.out.println(ans);
+  //     }
+  //     return;
+  //   }
+  //   // work: recursion
+  //   // Yes Choice
+  //   findSubset(str, ans+str.charAt(i), i+1);
+  //   // No Choice
+  //   findSubset(str, ans, i+1);
+  // }
+  // public static void main(String args[]) {
+  //   String str = "abc";
+  //   findSubset(str, "", 0);
+  // }
+
+  // find and print permutations
+  public static void findPermutation(String str, String ans) {
+    // base case
+    if (str.length() == 0) {
+      System.out.println(ans);
+      return;
+    }
+    for (int i = 0; i < str.length(); i++) {
+      char curr = str.charAt(i);
+      // Substring method cover in String.
+      // Substring e.g: "abcde" => "ab" + "de": "abde", remove c. (Read:
+      // Substring method)
+      String Newstr = str.substring(0, i) + str.substring(i + 1);
+      findPermutation(Newstr, ans + curr);
+    }
+
+    // work: recursion
+  }
+  public static void main(String args[]) {
+    String str = "abc";
+    findPermutation(str, "");
+  }
 }
