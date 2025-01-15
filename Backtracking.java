3fa905349c05c511e8897186510dab234158d500
Devangini Singh <84927553+Devanginisingh2002@users.noreply.github.com>
2025-01-15 18:14:54 +0000

    Backtracking

--- Backtracking.java
+++ Backtracking.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class file1 {
+public class Backtracking {
   /*
   //  Backtracking on Arrays
   public static void changeArr(int arr[], int i, int val) {
@@ -51,25 +51,86 @@ public class file1 {
   // }
 
   // find and print permutations
-  public static void findPermutation(String str, String ans) {
+  // public static void findPermutation(String str, String ans) {
+  //   // base case
+  //   if (str.length() == 0) {
+  //     System.out.println(ans);
+  //     return;
+  //   }
+  //   for (int i = 0; i < str.length(); i++) {
+  //     char curr = str.charAt(i);
+  //     // Substring method cover in String.
+  //     // Substring e.g: "abcde" => "ab" + "de": "abde", remove c. (Read:
+  //     // Substring method)
+  //     String Newstr = str.substring(0, i) + str.substring(i + 1);
+  //     findPermutation(Newstr, ans + curr);
+  //   }
+
+  //   // work: recursion
+  // }
+  // public static void main(String args[]) {
+  //   String str = "abc";
+  //   findPermutation(str, "");
+  // }
+
+  // n Queens question:
+  public static boolean isSafe(char board[][], int row, int col) {
+    // vertical up
+    for (int i = row - 1; i >= 0; i--) {
+      if (board[i][col] == 'Q') {
+        return false;
+      }
+    }
+    // diagonal left up
+    for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+      if (board[i][j] == 'Q') {
+        return false;
+      }
+    }
+    // diagonal right up
+    for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+      if (board[i][j] == 'Q') {
+        return false;
+      }
+    }
+    return true;
+  }
+  public static void nQueens(char board[][], int row) {
     // base case
-    if (str.length() == 0) {
-      System.out.println(ans);
+    if (row == board.length) {
+      // printBoard(board);
+      count++;  //optimization
       return;
     }
-    for (int i = 0; i < str.length(); i++) {
-      char curr = str.charAt(i);
-      // Substring method cover in String.
-      // Substring e.g: "abcde" => "ab" + "de": "abde", remove c. (Read:
-      // Substring method)
-      String Newstr = str.substring(0, i) + str.substring(i + 1);
-      findPermutation(Newstr, ans + curr);
+    // column loop
+    for (int j = 0; j < board.length; j++) {
+      if (isSafe(board, row, j)) {
+        board[row][j] = 'Q';
+        nQueens(board, row + 1); // function call
+        board[row][j] = 'x';     // Backtracking step
+      }
     }
-
-    // work: recursion
   }
+  public static void printBoard(char board[][]) {
+    System.out.println("------Chess Board-------");
+    for (int i = 0; i < board.length; i++) {
+      for (int j = 0; j < board.length; j++) {
+        System.out.print(board[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
+  static int count = 0;  //optimization 
   public static void main(String args[]) {
-    String str = "abc";
-    findPermutation(str, "");
+    int n = 4;
+    char board[][] = new char[n][n];
+    // initalise
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        board[i][j] = 'x';
+      }
+    }
+    nQueens(board, 0);
+    System.out.println("Total ways to solve nQueen: " + count);
   }
 }
