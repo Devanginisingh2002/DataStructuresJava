781b40e6eeca918c4797c5980a26624bb8479a50
Devangini Singh <84927553+Devanginisingh2002@users.noreply.github.com>
2025-03-04 17:33:39 +0000

    Bitwise

--- Bitwise.java
+++ Bitwise.java
@@ -111,7 +111,7 @@ public class Bitwise {
    } */
 
   // Count Set Bits in a Number
-  /* public static int countsetBit(int n){
+   public static int countsetBit(int n){
      int count = 0;
      while(n > 0){
          if((n & 1) != 0){    // check LSB
@@ -123,7 +123,7 @@ public class Bitwise {
  }
  public static void main(String args[]){
      System.out.println(countsetBit(16));
- } */
+ } 
 
   // Fast Exponentiation
   public static int FastExponentiation(int a, int n) {
